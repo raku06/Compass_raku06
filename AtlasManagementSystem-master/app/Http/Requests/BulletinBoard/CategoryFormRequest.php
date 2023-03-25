c7e86f0bb6869b714ccd5cee7ca42400941bcae6
Raku <raku.clover@gmail.com>
2023-03-25 16:06:12 +0900

    0325

---
+++ AtlasManagementSystem-master/app/Http/Requests/BulletinBoard/CategoryFormRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\BulletinBoard;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'main_category_name' => ['required', 'string', 'max:100', 'unique:main_categories,main_category'],
+            'main_category_id' => ['required', 'exists:sub_categories,main_category_id'],
+            'sub_category_name' => ['required', 'string', 'max:100', 'unique:sub_categories,sub_category'],
+        ];
+    }
+
+    public function messages(){
+        return [
+            'main_category_name.max' => 'メインカテゴリーは100文字以内で入力してください。',
+            'main_category_name.unique' => '同じ名前のメインカテゴリーは登録できません。',
+            'main_category_id.exists' => 'メインカテゴリーが登録されていません。',
+            'sub_category_name.max' => 'サブカテゴリーは100文字以内で入力してください。',
+            'sub_category_name.unique' => '同じ名前のサブカテゴリーは登録できません。',
+
+            // 必須事項
+            'main_category_name.required' => 'メインカテゴリーを入力してください。',
+            'sub_category_name.required' => 'サブカテゴリーを入力してください。',
+        ];
+    }
+}
