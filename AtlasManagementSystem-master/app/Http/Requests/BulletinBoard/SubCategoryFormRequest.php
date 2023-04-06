cc6029c9ede42a678e2370b3cf0f106d736d2699
Raku <raku.clover@gmail.com>
2023-04-07 00:19:37 +0900

    0406

--- AtlasManagementSystem-master/app/Http/Requests/BulletinBoard/SubCategoryFormRequest.php
+++ AtlasManagementSystem-master/app/Http/Requests/BulletinBoard/SubCategoryFormRequest.php
@@ -25,7 +25,7 @@ class SubCategoryFormRequest extends FormRequest
     {
         return [
             // 'main_category_name' => ['required', 'string', 'max:100', 'unique:main_categories,main_category'],
-            'main_category_id' => ['required', 'exists:sub_categories,main_category_id'],
+            'main_category_id' => ['required', 'exists:main_categories,id'],
             'sub_category_name' => ['required', 'string', 'max:100', 'unique:sub_categories,sub_category'],
         ];
     }
