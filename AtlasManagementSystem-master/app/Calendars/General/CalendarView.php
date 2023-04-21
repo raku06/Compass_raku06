24cd628d4cabad272be745e25ee03194086ffbb5
Raku <raku.clover@gmail.com>
2023-04-21 16:35:59 +0900

    0421

--- AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
+++ AtlasManagementSystem-master/app/Calendars/General/CalendarView.php
@@ -70,7 +70,7 @@ class CalendarView{
             $html[] = '<p class="m-auto p-0 w-75" style="font-size:13px">'.$reservePart.'参加</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }else{
-            $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
+            $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'" onclick="return confirm(\'予約日：'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'\n\n時間：'.$reservePart.'\n\n上記の予約をキャンセルしてもよろしいですか？\')">'. $reservePart .'</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }
         // elseif条件追加
