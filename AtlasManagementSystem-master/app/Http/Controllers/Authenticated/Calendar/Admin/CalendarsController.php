cf5808063b7e6e7c5c5926a1b7a3ea51c33544f4
Raku <raku.clover@gmail.com>
2023-05-19 18:07:25 +0900

    0519-2

--- AtlasManagementSystem-master/app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
+++ AtlasManagementSystem-master/app/Http/Controllers/Authenticated/Calendar/Admin/CalendarsController.php
@@ -21,6 +21,7 @@ class CalendarsController extends Controller
 
     public function reserveDetail($user_id = 0, $date, $part){
         $reservePersons = ReserveSettings::with('users')->where('setting_reserve', $date)->where('setting_part', $part)->get();
+        // dd($reservePersons);
         return view('authenticated.calendar.admin.reserve_detail', compact('reservePersons', 'date', 'part'));
     }
 
